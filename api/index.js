bc2050b14c39ec98679d3c624790288c6e5b272a
oussama bedraoui <mo_bedraoui@esi.dz>
2026-05-10 19:14:31 +0100

    test: add GET endpoint to test routing

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 module.exports = (req, res) => {
-  // Test barebones endpoint
+  // Test barebones GET endpoint
+  if (req.url === "/api/test-get" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify({ success: true, message: "GET works" }));
+  }
+
+  // Test barebones OPTIONS endpoint
   if (req.url === "/api/test-options" && req.method === "OPTIONS") {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
