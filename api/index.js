3d7ab69cde329723d5c78832592f39e5f1dda172
oussama bedraoui <mo_bedraoui@esi.dz>
2026-05-10 19:13:48 +0100

    test: add barebones OPTIONS test endpoint

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 module.exports = (req, res) => {
-  // Load and delegate everything to Express app
+  // Test barebones endpoint
+  if (req.url === "/api/test-options" && req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.writeHead(204);
+    return res.end();
+  }
+
+  // Load and delegate everything else to Express app
   const serverless = require("serverless-http");
   const app = require("../backend/src/app");
   const handler = serverless(app);
